ffdb19bc55e0054eec5cf20d0b16b906295c7c88
Paul Hirsch <paul.hirsch@gsa.gov>
2023-06-02 03:07:15 -0500

    Half way to da proxy zone

--- terraform/alpha/https-proxy.tf
+++ terraform/alpha/https-proxy.tf
@@ -1,13 +1,10 @@
 module "https-proxy" {
-  source        = "../https-proxy"
+  source        = "../modules/https-proxy"
   name          = "https-proxy"
-  cf_org_name   = var.cf_org_name               # gsa-tts-oros-fac
-  cf_space_name = "${var.cf_space_name}-egress" # eg prod-egress
-  client_space  = var.cf_space_name             # eg prod
+  cf_org_name   = var.cf_org_name
+  cf_space_name = "${var.cf_space_name}-egress"
+  client_space  = var.cf_space_name
   instances     = var.https_proxy_instances
-
-  # head of the "main" branch as of this commit
-  gitref = "7487f882903b9e834a5133a883a88b16fb8b16c9"
 
   allowlist = {
     clubfed_deps = [
